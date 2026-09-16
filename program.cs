1ac7d154c09b979e64b0d25b8e66f7f534e5bb40
aazab2669-rgb <aazab2669@gmail.com>
2026-09-16 11:51:19 +0000

    save project

--- program.cs
+++ program.cs
@@ -1,1 +1,73 @@
+namespace FristProject
+{
+    public class TaskTraker
+    {
+         static string[] Tasks = new string[10];
+         static int TaskIndex= 0;
+         public static void Main()
+        {
+            Console.WriteLine("Welcome  ");
+            Console.WriteLine("------------");
+            Console.WriteLine("Enter from 1 to 5 only");
 
+            while (true)
+            {
+            string UserChoice = Console.ReadLine();
+            switch (UserChoice)
+            {
+                case "1":
+                AddTask();
+                break;
+
+                case "2":
+                ViewTasks();
+                break;
+
+                case "3":
+                CompletedTasks();
+                break;
+
+                case "4":
+                RemoveTask();
+                break; 
+
+                case "5":
+                Environment.Exit(0);
+                break;
+
+                default:
+                Console.WriteLine("Enrer a number from 1 to 5 only");
+                break;
+            }
+            }        
+        }
+         public static void AddTask()
+        {
+           Console.WriteLine("Enter Task Title");
+           string TaskTitle = Console.ReadLine();
+           Tasks[TaskIndex]=TaskTitle;
+            ++TaskIndex;
+            Console.WriteLine("Task added");
+        }
+         public static void ViewTasks()
+        {
+            Console.WriteLine(" Tasks List : ");
+            for (int i = 0; i < TaskIndex; i++)
+            {
+               Console.WriteLine($"{i+1} TaskTitle : {Tasks[i]}") ;
+            }
+        }
+         public static void CompletedTasks()
+        {
+            Console.WriteLine("TaskNumber");
+            int TaskId = Convert.ToInt32(Console.ReadLine());
+            Tasks[TaskId-1]= Tasks[TaskId-1] + ": is COMPLETED";
+        }
+         public static void RemoveTask()
+        {
+            Console.WriteLine("TaskNumber");
+            int TaskId = Convert.ToInt32(Console.ReadLine());
+            Tasks[TaskId-1]= " "; 
+        }
+    }
+}
